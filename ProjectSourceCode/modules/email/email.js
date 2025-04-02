0b6f0dc360a1831c3715b5464e57c971d4f13ce7
Micah Gagerman <micahgagerman21@gmail.com>
2025-04-01 21:26:53 -0600

    setup event loop

--- ProjectSourceCode/modules/email/email.js
+++ ProjectSourceCode/modules/email/email.js
@@ -14,7 +14,7 @@ const transporter = nodemailer.createTransport({
 });
 // custom functions
 
-async function sendEmail(to, subject, text, html = null) {
+async function sendEmail(to, subject, text, html) {
     try {
       const mailOptions = {
         from: process.env.GMAIL_ADDRESS,
@@ -29,9 +29,60 @@ async function sendEmail(to, subject, text, html = null) {
       console.error('Error sending email:', e);
     }
 }
+
+
+let events = [];
+
+
+// upon creation of an event, send a confirmation email to all recipients
+let sendConformationEmail = async (userEmails,event_name,event_time,reminder_time) => {
+  subject = `New Event: ${event_name}`
+  html = `<div>Your event ${event_name} starts at ${event_time}<div>`
+  // go recipient each email and send the email :O
+  userEmails.forEach(email => sendEmail(email,subject,"",html));
+  // add event
+  events.push({
+    userEmails: userEmails,
+    event_name,
+    event_time,
+    reminder_time
+  });
+} 
+
+
+// done automatically in event loop, do not export
+let sendReminderEmail = async (userEmails,event_name,event_time,reminder_time) => {
+  //@todo
+}
+
+
+
+
+
+
+
+let EmailEventLoop = async() => {
+  const date = new Date();
+  setInterval(()=>{
+    // get time
+    let currTime = date.getTime() 
+    // check if n minutes before someones event
+    for(let i = 0; i<events.length; i++) {
+      if(events[i].event_time-reminder_time <= currTime) {
+        // send email to each recipient
+        sendReminderEmail(events[i].userEmails,event_name,event_time,reminder_time);
+        // remove event from set
+        events.splice(i,1);
+      }
+    }
+  },60000)//repeat every minute
+}
+
+
+
+
 (async ()=>{
     // Note: this wont work with CU affiliated emails
-    await sendEmail("micahgagerman21@gmail.com","Subject","preview text maybe??",`<div id="email_body"><h1>Cool awesome title</h1><p>this will soon be a really cool email body</p></div>`);
 })()
 
 // // export functions
