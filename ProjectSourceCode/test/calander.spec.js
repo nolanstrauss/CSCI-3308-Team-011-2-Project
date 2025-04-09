53878d6160d89dc5940dfe09ed15c55d38b3751e
Charbel Adwan <charbel.adwan@colorado.edu>
2025-04-09 10:26:02 -0600

    Update to Calandar Test

--- ProjectSourceCode/test/calander.spec.js
+++ ProjectSourceCode/test/calander.spec.js
@@ -6,18 +6,15 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-// Import your Express app; ensure that index.js exports just the app (not app.listen(3000))
-// so that tests can import it without starting a separate server instance.
 const app = require('../index');
 
 describe('GET /calendar (Unauthenticated)', () => {
   it('should redirect unauthenticated users to /login', (done) => {
     chai.request(app)
       .get('/calendar')
-      .redirects(0)  // Prevent chai-http from automatically following the redirect
+      .redirects(0)  
       .end((err, res) => {
         expect(res).to.have.status(302);
-        // Check that the Location header points to the login route.
         expect(res).to.have.header('location', '/login');
         done();
       });
