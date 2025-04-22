6e2c273ec8585113a2ba661e6e4d840229f49221
cysteely <cstyle@gmail.com>
2025-04-22 08:32:35 -0600

    changing variable names back

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -7,19 +7,19 @@ CREATE TABLE users
 CREATE TABLE users_to_events
 (
     username VARCHAR(50),
-    eventID VARCHAR(50)
+    eventid VARCHAR(50)
 );
 
 CREATE TABLE events
 (
-    eventID Serial PRIMARY KEY,
-    eventName VarChar(120) NOT NULL,
-    eventCategory VarChar (120),
-    eventDate VarChar(60) NOT NULL,
-    eventReminderDelay INT NOT NULL,
-    eventDescription TEXT,
-    eventLink VarChar(520),
-    eventEmailList VarChar(1000),
-    eventUser VARCHAR(50) NOT NULL
+    eventid Serial PRIMARY KEY,
+    eventname VarChar(120) NOT NULL,
+    eventcategory VarChar (120),
+    eventdate VarChar(60) NOT NULL,
+    eventreminderdelay INT NOT NULL,
+    eventdescription TEXT,
+    eventlink VarChar(520),
+    eventemaillist VarChar(1000),
+    eventuser VARCHAR(50) NOT NULL
 );
 
